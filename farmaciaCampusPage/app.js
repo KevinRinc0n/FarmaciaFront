6398f7a99a5332654401186022aa6d55d39c286c
KevinRinc0n <rinconkdavid@gmail.com>
2023-09-27 17:28:29 -0500

    feat: :sparkles: sparkles: new things added

---
+++ farmaciaCampusPage/app.js
@@ -0,0 +1,85 @@
+var cartas = document.getElementById("cards");
+var ocultarMain = document.getElementById("main");
+var medisCompras = document.getElementById("mediCompras");
+var empleCompras = document.getElementById("litEmpleCompras");
+
+
+var medis = document.getElementById("listarMedis");
+medis.addEventListener("click", function(){
+    ocultarMain.style.display = "none";
+    medisCompras.style.display = "none";
+    empleCompras.style.display = "none";
+    cartas.style.display = "flex"
+    cartas.style.alignItems = "center"
+    cartas.style.justifyContent = "center"
+});
+
+var medicamentoComprado = document.getElementById("litMedisCompras");
+medicamentoComprado.addEventListener("click", function(){
+    cartas.style.display = "none";
+    empleCompras.style.display = "none";
+    ocultarMain.style.display = "none";
+    medisCompras.style.display = "block";
+});
+
+var medis = document.getElementById("listarMedis");
+medis.addEventListener("click", function(){
+    ocultarMain.style.display = "none";
+    medisCompras.style.display = "none";
+    cartas.style.display = "none";
+    cartas.style.display = "block"
+});
+
+var medis = document.getElementById("listarMedis");
+medis.addEventListener("click", function(){
+    cartas.style.display = "block"
+    ocultarMain.style.display = "none";
+});
+
+var medis = document.getElementById("listarMedis");
+medis.addEventListener("click", function(){
+    cartas.style.display = "block"
+    ocultarMain.style.display = "none";
+});
+
+var medis = document.getElementById("listarMedis");
+medis.addEventListener("click", function(){
+    cartas.style.display = "block"
+    ocultarMain.style.display = "none";
+});
+
+var medis = document.getElementById("listarMedis");
+medis.addEventListener("click", function(){
+    cartas.style.display = "block"
+    ocultarMain.style.display = "none";
+});
+
+var medis = document.getElementById("listarMedis");
+medis.addEventListener("click", function(){
+    cartas.style.display = "block"
+    ocultarMain.style.display = "none";
+});
+
+var medis = document.getElementById("listarMedis");
+medis.addEventListener("click", function(){
+    cartas.style.display = "block"
+    ocultarMain.style.display = "none";
+});
+
+var medis = document.getElementById("listarMedis");
+medis.addEventListener("click", function(){
+    cartas.style.display = "block"
+    ocultarMain.style.display = "none";
+});
+
+var medis = document.getElementById("listarMedis");
+medis.addEventListener("click", function(){
+    cartas.style.display = "block"
+    ocultarMain.style.display = "none";
+});
+
+var medis = document.getElementById("listarMedis");
+medis.addEventListener("click", function(){
+    cartas.style.display = "block"
+    ocultarMain.style.display = "none";
+});
